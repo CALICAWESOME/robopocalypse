0a73a1932397a7f76c83aa96c195bc6d0223e27d
CALICAWESOME <greendolphinavenue@gmail.com>
2018-03-26 14:16:50 -0400

    add ethernet text code

---
+++ arduinoFuckaround/ethTest/ethTest.ino
@@ -0,0 +1,53 @@
+#include<SPI.h>
+#include<Ethernet.h>
+
+byte mac[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
+char server[] = "www.bobby-mart.in";
+
+// in case DHCP fucks up
+IPAddress ip(192, 168, 0, 177);
+
+// YAWOUEWEORHWE
+EthernetClient client;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  while(!Serial);
+
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("NO DHCP BITCH");
+    Ethernet.begin(mac, ip);
+  }
+  delay(1000);
+  Serial.println("connecting...?");
+
+  if (client.connect(server, 80)) {
+    Serial.println("connected");
+    // LET'S MAKE A REQUEST YAY
+    client.println("GET http://bobby-mart.in/ HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  else {
+    Serial.println("connection FAILED BITCH");
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // NOTTINFD
+    while (true);
+  }
+}
