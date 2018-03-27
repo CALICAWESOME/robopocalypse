560bb32cc4acec1a5f9b8207cd141f739cfe105d
Alex Robbins <robbinsa@wit.edu>
2018-03-27 16:40:24 -0400

    changed library

--- arduinoFuckaround/socketo/socketo.ino
+++ arduinoFuckaround/socketo/socketo.ino
@@ -1,24 +1,68 @@
-#include "Arduino.h"
-#include <Ethernet.h>
 #include <SPI.h>
+#include <Ethernet.h>
+
 #include <WebSocketClient.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-char server[] = "echo.websocket.org";
-WebSocketClient client;
+EthernetClient client;
+WebSocketClient webSocketClient;
+
+//change to your mac address
+byte mac[]= { 0x90, 0xA2, 0xDA, 0x11, 0x22, 0x34 };
 
 void setup() {
   Serial.begin(19200);
-  Ethernet.begin(mac);
-  client.connect(server);
-  client.setDataArrivedDelegate(dataArrived);
-  client.send("Hello World!");
+  Serial.println(F("Websocketclient starting"));
+  Ethernet.begin(mac); // initialize ethernet
+  Serial.println(Ethernet.localIP()); // printout IP address for debug purposes
+  delay(300); // this is arduino baby ;-)
+
+  // Connect to the websocket server
+  if (client.connect("echo.websocket.org", 80)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
+    while(1) {
+      // Hang on failure
+    }
+  }
+
+  // Handshake with the server
+  webSocketClient.path = "/";
+  webSocketClient.host = "echo.websocket.org";
+  
+  if (webSocketClient.handshake(client)) {
+    Serial.println("Handshake successful");
+  } else {
+    Serial.println("Handshake failed.");
+    while(1) {
+      // Hang on failure
+    }  
+  }
 }
 
 void loop() {
-  client.monitor();
-}
+  String data;
+  
+  if (client.connected()) {
+    
+    webSocketClient.getData(data);
 
-void dataArrived(WebSocketClient client, String data) {
-  Serial.println("Data Arrived: " + data);
+    if (data.length() > 0) {
+      Serial.print("Received data: ");
+      Serial.println(data);
+    }
+
+    // capture the value of analog 0, send it along
+    data = String(analogRead(0));
+    Serial.println("Sending "+data);
+    webSocketClient.sendData(data);
+  } else {    
+    Serial.println("Client disconnected.");
+    while (1) {
+      // Hang on disconnect.
+    }
+  }
+  
+  // wait to fully let the client disconnect
+  delay(1000);
 }
