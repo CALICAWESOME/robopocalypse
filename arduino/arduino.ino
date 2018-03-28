16417a7998f08ebb0522ba2687ed5d01f464eb78
CALICAWESOME <greendolphinavenue@gmail.com>
2018-03-28 11:14:19 -0400

    get rid of some stuff in arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -3,10 +3,16 @@
  */
 #include <SPI.h>
 #include <Ethernet.h>
+#include <Servo.h>
 
 const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 const char server[] = "www.bobby-mart.in";
 const int port = 6969;
+
+String posString = "";
+int pos = 0;
+
+Servo servo;
 
 IPAddress ip(192, 168, 0, 177);
 
@@ -16,13 +22,23 @@ void setup() {
   Serial.begin(19200);
   Ethernet.begin(mac);
   client.connect(server, port);
-  client.println("HELLO PLEASE");
+
+  servo.attach(9);
+  servo.write(pos);
 }
 
 void loop() {
   if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+    char inChar = client.read();
+    if (isDigit(inChar)) {
+      posString += inChar;
+    }
+    if (inChar == '\n') {
+      pos = posString.toInt();
+      Serial.println(pos);
+      servo.write(pos);
+      posString = "";
+    }
   }
 
   if (!client.connected()) {
