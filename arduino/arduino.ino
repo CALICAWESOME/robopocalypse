a88b7f57208a91a93116370314b2e2ab63e74781
CALICAWESOME <greendolphinavenue@gmail.com>
2018-03-28 01:30:42 -0400

    FUCK FUCKAROUND

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,22 +1,36 @@
 /*
  * Bobby Martin & Alex Robbins
- * 
- * This is a placeholder sketch that sets a servo motor on
- * pin 9 to a random value from 0 to 180 every second.
  */
-#include <Servo.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
-long randomNumber;
-Servo servo;
+const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+const char server[] = "www.bobby-mart.in";
+const int port = 6969;
+
+IPAddress ip(192, 168, 0, 177);
+
+EthernetClient client;
 
 void setup() {
-  // Serial.begin(19200);
-  
-  servo.attach(9);
-  randomSeed(analogRead(0));
+  Serial.begin(19200);
+  Ethernet.begin(mac);
+  client.connect(server, port);
+  client.println("HELLO PLEASE");
 }
 
 void loop() {
-  servo.write(random(0, 180));
-  delay(1000);
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
 }
--- webserver/index.js
+++ webserver/index.js
@@ -6,23 +6,34 @@
 const ws = require('ws');
 const net = require('net');
 
-const webpageSocket = new ws.Server({
+const dummySocket = {
+    write: () => {}
+};
+
+let arduinoSocket = dummySocket;
+
+const webpageSocketServer = new ws.Server({
     port: 6968
 }, () => {
     console.log('Webpage socket server listening on port 6968');
 });
-
-webpageSocket.on('connection', socket => {
+webpageSocketServer.on('connection', socket => {
     console.log('Webpage client connected!');
     // set up listeners
     socket.on('close', (code, reason) => console.log(`Webpage client disconnected because: ${reason}`));
-    socket.on('message', console.log);
+    socket.on('message', message => {
+        console.log(message);
+        arduinoSocket.write(message);
+    });
 });
 
 const arduinoSocketServer = new net.createServer(socket => {
     console.log('Somebody connected!!');
     // set up listeners
-    socket.on('close', hadError => console.log(`Disconnected ${hadError ? 'with error' : 'without error'}`));
-    socket.on('data', data => socket.write(data));
+    socket.on('close', hadError => {
+        console.log(`Disconnected ${hadError ? 'with error' : 'without error'}`);
+        arduinoSocket = dummySocket;
+    });
+    arduinoSocket = socket;
 });
 arduinoSocketServer.listen(6969, () => console.log('Arduino socket server listening on port 6969'));
